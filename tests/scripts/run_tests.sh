6b454762fee2dcfbc38c024efa46e6f3eba2d4b2
Yatin Manerkar <manerkar@princeton.edu>
2017-06-20 19:21:17 -0400

    Tutorial: Add run_tests Python script and tweak dir structure

--- tests/scripts/run_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ "$#" -ne 2 ]; then
-    echo "Usage: ./run_tests.sh <test_dir> <uarch_name>"
-fi
-
-for file in $(find $1 -name \*.test ); do
-    echo "Running $file";
-    ../../src/pipecheck -i $file -o out/$(basename $file).out -m ../../uarches/tutorial/$2.uarch;
-done
